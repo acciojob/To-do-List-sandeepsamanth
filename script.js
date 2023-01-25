58d3569c5ebab2eb1bf198bbb9d737532b8bc9df
sandeepsamanth <98205088+sandeepsamanth@users.noreply.github.com>
2023-01-25 12:16:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
 
+const input = document.querySelector('#newTodoInput');
+const button = document.querySelector('#addTodoBtn');
+const todoList = document.querySelector('#todoList');
+
+button.addEventListener('click', function() {
+  if(input.value.trim() != ""){
+    const newTodo = document.createElement('li');
+    newTodo.innerText = input.value;
+    todoList.appendChild(newTodo);
+    input.value = "";
+  }
+});
